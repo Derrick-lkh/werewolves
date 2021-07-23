980c0f7d31332e6a9c0b84700de517fbce7774d9
derrickSP <derricklimkaihao7.17@ichat.sp.edu.sg>
2021-07-24 00:19:50 +0800

    message update

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
 
 @bot.message_handler(commands = ['greet','start'])
 def greet(message):
-    msg = ''' C4X IS THE WORST '''
+    msg = ''' C4X IS THE WORST'''
     bot.reply_to(message, msg)
 
 
