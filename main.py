d3f372aa5366be8a8dd5574e67c0ba716d5d6e34
derrickSP <derricklimkaihao7.17@ichat.sp.edu.sg>
2021-07-24 00:32:14 +0800

    main.py update

--- main.py
+++ main.py
@@ -7,15 +7,14 @@ load_dotenv()
 token = os.getenv('AUT_TOKEN')
 bot = telebot.TeleBot(token)
 
-
-def main():
-    bot.polling()
-
-
 @bot.message_handler(commands = ['greet','start'])
 def greet(message):
     msg = ''' C4X IS THE WORST'''
     bot.reply_to(message, msg)
+
+
+def main():
+    bot.polling(none_stop=False, interval=0, timeout=20)
 
 
 if __name__ == '__main__':
