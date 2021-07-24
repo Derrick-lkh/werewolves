f73307d67c9f3265b5167f89ae2ca875cb699269
Aaronpeh022 <87894275+Aaronpeh022@users.noreply.github.com>
2021-07-25 02:14:47 +0800

    Added some win conditions :D

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ load_dotenv()
 token = os.getenv('AUT_TOKEN')
 bot = telebot.TeleBot(token)
 
-
-Users = {376517278: {'user_name': 'Derrick_lkh'}, 620331844: {'user_name': 'Valentino'}, 33540344: {'user_name': 'Mant'}, 341249922: {'user_name': 'AaronPeh'}}
-room_id = ""
+# Users = {376517278: {'user_name': 'Derrick_lkh'}, 620331844: {'user_name': 'Valentino'}, 33540344: {'user_name': 'Mant'}, 341249922: {'user_name': 'AaronPeh'}}
+Users = {33540344: {'user_name': 'Mant'}, 341249922: {'user_name': 'AaronPeh'}, 376517278: {'user_name': 'Derrick_lkh'}}
+room_id = "-530196332"
 Roles = ["Hacker", "FBI", "FBI", "Sage", "Shield", "Civilian", "Civilian", "Civilian", "Civilian"]
 Roles1 = ["FBI", "Hacker", "FBI", "Sage", "Shield", "Civilian", "Civilian", "Civilian", "Civilian"]
 reset_markup = types.ReplyKeyboardRemove(selective=False)
@@ -94,6 +94,10 @@ collated_votes = []
 
 def vote_result():
     bot.send_message(room_id, "End of Voting", reply_markup=reset_markup)
+    if len(collated_votes) == 0:
+        msg = "TIED! No one was Yeeted \U0001f4a9"
+        bot.send_message(room_id, msg)
+        return
     collated_votes.sort()
     highest_count_user = 0
     highest_count = 0
@@ -121,6 +125,28 @@ def vote_result():
     bot.send_message(room_id, msg)
 
 
+#Win condition
+noOfHackers = 0
+def checkCondition():
+    global noOfHackers
+    noOfHackers = 0
+    noOfSurvivors = 0
+    print("Checking win condition")
+    for i in Users:
+        print(i)
+        if "dead" not in Users[i].keys():
+            noOfSurvivors += 1
+            if Users[i]["roles"] == "Hacker":
+                noOfHackers += 1
+    if noOfHackers / noOfSurvivors > 0.49:
+        msg = "Hacker won"
+    elif noOfHackers == 0:
+        msg = "Defenders won"
+    else:
+        msg = str(noOfHackers)+ "hackers remain"
+    print(msg)
+    bot.send_message(room_id, msg)
+    return msg
 
 
 @bot.message_handler(commands=['vote'])
@@ -144,6 +170,7 @@ def hack(message):
 def init_room(message):
     global room_id
     room_id = message.chat.id
+    print(room_id)
     msg = '''**C4X Werewolves Started**\nUse /join to enter the game'''
     bot.reply_to(message, msg)
 
@@ -159,26 +186,30 @@ def join_game(message):
     else:
         msg = user_name + " Already in the game"
     print(Users)
+    print(chat_id)
     bot.reply_to(message, msg)
 
 
 # Starts game
 @bot.message_handler(commands=['start_game'])
 def start_game(message):
+    print("Started game")
     global voting_allowed
     msg = '''Starting Game ... ...'''
     bot.reply_to(message, msg)
     random_roles()
 
     night_actions()
-    time.sleep(20)
+    time.sleep(10)
     collate_night_actions()
+    checkCondition()
     # Enable voting - initiate voting
     voting_allowed = True
     voting_phase()
-    time.sleep(20)
+    time.sleep(10)
     voting_allowed = False
     vote_result()
+    checkCondition()
     # Disable voting - End of voting
 
 
